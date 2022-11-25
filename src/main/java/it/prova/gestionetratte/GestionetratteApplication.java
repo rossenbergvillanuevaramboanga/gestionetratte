27b070d5f001db151039df0932c663ff221d1580
Rossenberg Villanueva Ramboanga <rossenbergvillanueva.ramboanga@gmail.com>
2022-11-25 14:21:17 +0100

    first commit

---
+++ src/main/java/it/prova/gestionetratte/GestionetratteApplication.java
@@ -0,0 +1,13 @@
+package it.prova.gestionetratte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionetratteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionetratteApplication.class, args);
+	}
+
+}
