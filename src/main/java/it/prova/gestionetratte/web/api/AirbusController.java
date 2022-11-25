367a519b0aec74299437e71994b3f4d6712b5da5
Rossenberg Villanueva Ramboanga <rossenbergvillanueva.ramboanga@gmail.com>
2022-11-25 16:50:32 +0100

    Implemneted start controller Airbus

--- src/main/java/it/prova/gestionetratte/web/api/AirbusController.java
+++ src/main/java/it/prova/gestionetratte/web/api/AirbusController.java
@@ -3,9 +3,14 @@ package it.prova.gestionetratte.web.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.prova.gestionetratte.dto.AirbusDTO;
@@ -33,4 +38,28 @@ public class AirbusController {
 			throw new AirbusNotFoundException("Airbus not found con id: " + id);
 		return AirbusDTO.buildAirbusDTOFromModel(airbus, true);
 	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public AirbusDTO createNew() {
+		return null; 
+	}
+	
+	@PutMapping("/{id}")
+	public AirbusDTO update() {
+		return null; 
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete() {
+		
+	}
+	
+	@PostMapping("/search")
+	public List<AirbusDTO> search(){
+		return null; 
+	}
+	
+	
+	
 }
