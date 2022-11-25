5a9b7eff3ff1087f8bf6ddbbddd52157884256c6
Rossenberg Villanueva Ramboanga <rossenbergvillanueva.ramboanga@gmail.com>
2022-11-25 14:40:49 +0100

    Implemented Airbus model

--- src/main/java/it/prova/gestionetratte/model/Airbus.java
+++ src/main/java/it/prova/gestionetratte/model/Airbus.java
@@ -1,5 +1,120 @@
 package it.prova.gestionetratte.model;
 
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "airbus")
 public class Airbus {
+	
+	//Airbus (id, codice, descrizione, dataInizioServizio, numeroPasseggeri, tratte)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	@Column(name = "codice")
+	private String codice;
+	@Column(name = "descrizione")
+	private String descrizione;
+	@Column(name = "datainizioservizio")
+	private LocalDate dataInizioServizio;
+	@Column(name = "numeropasseggeri")
+	private Integer numeroPasseggeri;
+	@Column(name = "id")
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "airbus")
+	private Set<Tratta> tratte = new HashSet<Tratta>(0);
+	
+	public Airbus() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Airbus(Long id, String codice, String descrizione, LocalDate dataInizioServizio, Integer numeroPasseggeri,
+			Set<Tratta> tratte) {
+		super();
+		this.id = id;
+		this.codice = codice;
+		this.descrizione = descrizione;
+		this.dataInizioServizio = dataInizioServizio;
+		this.numeroPasseggeri = numeroPasseggeri;
+		this.tratte = tratte;
+	}
+	
+	
+	public Airbus(String codice, String descrizione, LocalDate dataInizioServizio, Integer numeroPasseggeri) {
+		super();
+		this.codice = codice;
+		this.descrizione = descrizione;
+		this.dataInizioServizio = dataInizioServizio;
+		this.numeroPasseggeri = numeroPasseggeri;
+	}
+
+	public Airbus(Long id, String codice, String descrizione, LocalDate dataInizioServizio, Integer numeroPasseggeri) {
+		super();
+		this.id = id;
+		this.codice = codice;
+		this.descrizione = descrizione;
+		this.dataInizioServizio = dataInizioServizio;
+		this.numeroPasseggeri = numeroPasseggeri;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCodice() {
+		return codice;
+	}
+
+	public void setCodice(String codice) {
+		this.codice = codice;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public LocalDate getDataInizioServizio() {
+		return dataInizioServizio;
+	}
+
+	public void setDataInizioServizio(LocalDate dataInizioServizio) {
+		this.dataInizioServizio = dataInizioServizio;
+	}
+
+	public Integer getNumeroPasseggeri() {
+		return numeroPasseggeri;
+	}
+
+	public void setNumeroPasseggeri(Integer numeroPasseggeri) {
+		this.numeroPasseggeri = numeroPasseggeri;
+	}
+
+	public Set<Tratta> getTratte() {
+		return tratte;
+	}
+
+	public void setTratte(Set<Tratta> tratte) {
+		this.tratte = tratte;
+	}
+	
 
 }
