d95c705075dacd736ee71858b310e0b84c519f91
Rossenberg Villanueva Ramboanga <rossenbergvillanueva.ramboanga@gmail.com>
2022-11-25 19:05:54 +0100

    Implemented Tratta Controller

--- src/main/java/it/prova/gestionetratte/web/api/TrattaController.java
+++ src/main/java/it/prova/gestionetratte/web/api/TrattaController.java
@@ -1,5 +1,77 @@
 package it.prova.gestionetratte.web.api;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import it.prova.gestionetratte.dto.TrattaDTO;
+import it.prova.gestionetratte.model.Tratta;
+import it.prova.gestionetratte.service.TrattaService;
+import it.prova.gestionetratte.web.api.exception.IdNotNullForInsertException;
+import it.prova.gestionetratte.web.api.exception.TrattaNotFoundException;
+@RestController
+@RequestMapping("api/tratta")
 public class TrattaController {
+	
+	@Autowired
+	private TrattaService trattaService;
+	
+	@GetMapping
+	public List<TrattaDTO> getAll(){
+		return TrattaDTO.createTrattaDTOListFromModelList(trattaService.listAllElements(true), true);
+		
+	}
+	
+	@PostMapping
+	public TrattaDTO createNew(@Valid @RequestBody TrattaDTO trattaInput) {
+		if(trattaInput.getId() != null)
+			throw new IdNotNullForInsertException("Non è ammesso fornire un id per la creazione");
+		Tratta trattaInserita = trattaService.inserisciNuovo(trattaInput.buildTrattaModel());
+		return TrattaDTO.buildTrattaDTOFromModel(trattaInserita, true);
+	}
+	
+	@GetMapping("/{id}")
+	public TrattaDTO findById(@PathVariable(value = "id", required = true) long id) {
+		Tratta tratta = trattaService.caricaSingoloElementoEager(id);
+		
+		if(tratta == null)
+			throw new TrattaNotFoundException("Tratta not found con id: " + id);
+		return TrattaDTO.buildTrattaDTOFromModel(tratta, true);
+	}
+	
+	@PutMapping("/{id}")
+	public TrattaDTO update(@Valid @RequestBody TrattaDTO trattaInput, @PathVariable(required = true) Long id) {
+		Tratta tratta = trattaService.caricaSingoloElemento(id);
+		
+		if(tratta == null)
+			throw new TrattaNotFoundException("Tratta not found con id: " + id);
+		trattaInput.setId(id);
+		Tratta trattaAggiornata = trattaService.aggiorna(trattaInput.buildTrattaModel());
+		return TrattaDTO.buildTrattaDTOFromModel(trattaAggiornata, false);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable(required = true) Long id) {
+		trattaService.rimuovi(id);
+	}
+	
+	@PostMapping("/search")
+	public List<TrattaDTO> search(@RequestBody TrattaDTO example) {
+		return TrattaDTO.createTrattaDTOListFromModelList(trattaService.findByExample(example.buildTrattaModel()), false);
+	}
+	
 
 }
